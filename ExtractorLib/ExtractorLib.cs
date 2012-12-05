5f3ba2156de14ec20298ffc6e6b2f1881fb162ec
Silas Snider <swsnider@gmail.com>
2012-12-05 11:42:03 -0800

    Fix lack of a required newline

--- ExtractorLib/ExtractorLib.cs
+++ ExtractorLib/ExtractorLib.cs
@@ -132,6 +132,7 @@ namespace ExtractorLib
                 }
                 outputFile.WriteLine("---");
                 serializer.Serialize(outputFile, table);
+                outputFile.WriteLine();
                 outputFile.WriteLine("...");
                 outputFile.Flush();
             }
